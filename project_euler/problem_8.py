e64be5c021a9ff8abfe249a4af4aa6a8d5dfb6ec
Javid Khasizada <javid.khasizada@gmail.com>
2021-08-26 01:07:25 +0300

    Problem 8 is solved

--- project_euler/problem_8.py
+++ project_euler/problem_8.py
@@ -22,3 +22,53 @@ Solution [pseudocode]
 TBA
 
 """
+from typing import Optional
+
+THOUSAND = 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450
+
+def find_biggest_product(number: Optional[int] = None, count_of_digits: Optional[int] = None) -> int:
+    if number is None or count_of_digits is None or count_of_digits < 1:
+        return -1
+
+    str_number = str(number)
+
+    position = find_position(str_number, count_of_digits)
+    str_digits = str_number[position:position + count_of_digits]
+    product = find_product(str_digits)
+    return product
+
+
+def find_position(number: Optional[str] = None, count_of_digits: Optional[int] = None) -> int:
+    if number is None or count_of_digits is None or count_of_digits < 1:
+        return -1
+
+    prod = 1
+    highest_pos = 0
+
+    for i, _ in enumerate(number[:count_of_digits * -1]):
+        curr_digits = number[i:i+count_of_digits]
+        curr_prod = find_product(curr_digits)
+        if curr_prod > prod:
+            prod = curr_prod
+            highest_pos = i
+
+    return highest_pos
+
+
+def find_product(digits: Optional[str] = None) -> int:
+    if digits is None:
+        return -1
+
+    curr_prod = 1
+
+    for i in digits:
+        curr_prod *= int(i)
+
+    return curr_prod
+
+
+prod = find_biggest_product(THOUSAND, 4)
+print(prod)
+
+prod = find_biggest_product(THOUSAND, 13)
+print(prod)
