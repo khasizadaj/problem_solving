d6bd54b9834294761189f485af4b1023959be41e
ekavajh <javid.khasizada@ericsson.com>
2023-06-25 08:49:56 -0600

    Add reading logic

--- adventofcode/day_1/c/main.c
+++ adventofcode/day_1/c/main.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 
 int	find_highest_calorie(t_str str);
+t_str	open_and_read(char *file_name);
 
 int	main(int argc, char **argv)
 {
 	t_str	content;
 	int		highest_calorie;
 	
-
 	if (argc == 2)
 	{
-		// TODO Implement reading logic; 
-		content = "1000\n2000\n3000\n\n8000\n1000\n\n10000\n";
-		// content = read_input();
+		content = open_and_read(argv[1]);
 		highest_calorie = find_highest_calorie(content);
 		printf("Highest calorie:\t%d\n", highest_calorie);
 	}
+	else
+		printf("Please provide input file.");
 }
